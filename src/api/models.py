ac2a53bd2ea88f3d8a7fd678217b5513bde7181b
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-08-27 18:49:47 +0000

    Creacion de la tabla de Administrator

--- src/api/models.py
+++ src/api/models.py
@@ -27,6 +27,13 @@ class Administrator(db.Model):
     last_name2 = db.Column(db.String(50), nullable=True)
     cardID_type = db.Column(db.String(25), nullable=False)
     number_cardID = db.Column(db.BigInteger, nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    phone_number = db.Column(db.BigInteger, nullable=False)
+    province = db.Column(db.String(50), nullable=False)
+    canton = db.Column(db.String(50), nullable=False)
+    district = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return 'Administrator: {}'.format(self.name)
@@ -38,5 +45,11 @@ class Administrator(db.Model):
             "last_name1": self.last_name1,
             "last_name2": self.last_name2,
             "cardID_type": self.cardID_type,
-            "number_cardID": self.number_cardID
+            "number_cardID": self.number_cardID,
+            "birthday": self.birthday,
+            "phone_number": self.phone_number,
+            "province": self.province,
+            "canton": self.canton,
+            "district": self.district,
+            "email": self.email
         }
