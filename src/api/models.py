e43f5d4c011ec5c59d9adfc181c497b95896df03
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-09-04 00:44:14 +0000

    Creacion de tablas de Payment e Invoice

--- src/api/models.py
+++ src/api/models.py
@@ -106,6 +106,23 @@ class Payment(db.Model):
     __tablename__ = 'payment'
 
     id = db.Column(db.Integer, primary_key=True)
+    pay = db.Column(db.String(15), nullable=False)
+    SINPE = db.Column(db.BigInteger, nullable=True)
+    IBAN = db.Column(db.String(50), nullable=True)
+
+    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'))
+    professor = db.relationship("Professor", back_populates="payment")
+
+    def __repr__(self):
+        return 'Payment: {}'.format(self.pay)
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "pay": self.pay,
+            "SINPE": self.SINPE,
+            "IBAN": self.IBAN
+        }
 
 class Student(db.Model):
     __tablename__ = 'student'
@@ -128,6 +145,8 @@ class Student(db.Model):
     password = db.Column(db.String(50), nullable=False)
     is_active = db.Column(db.Boolean(), nullable=False)
 
+    invoice = db.relationship("Invoice", backpopulates="student")
+
     def __repr__(self):
         return 'Student: {}'.format(self.name)
     
@@ -147,5 +166,35 @@ class Student(db.Model):
             "province": self.province,
             "canton": self.canton,
             "district": self.district,
+            "email": self.email
+        }
+    
+class Invoice(db.Model):
+    __tablename__ = 'invoice'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    last_name1 = db.Column(db.String(50), nullable=False)
+    last_name2 = db.Column(db.String(50), nullable=True)
+    cardID_type = db.Column(db.String(25), nullable=False)
+    number_cardID = db.Column(db.BigInteger, nullable=False)
+    phone_number = db.Column(db.BigInteger, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    studnet = db.relationship("Student", back_populates="invoice")
+
+    def __repr__(self):
+        return 'Invoice: {}'.format(self.name)
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "last_name1": self.last_name1,
+            "last_name2": self.last_name2,
+            "cardID_type": self.cardID_type,
+            "number_cardID": self.number_cardID,
+            "phone_number": self.phone_number,
             "email": self.email
         }
